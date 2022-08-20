9fefa873133cd40118950f26739509c8eebe15e5
Liu_yunnan <1792147770@qq.com>
2022-08-20 09:55:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+vue3笔记：https://xiaoman.blog.csdn.net/category_11618172_2.html
+nvm，nrm使用教程：https://blog.csdn.net/qq1195566313/article/details/125027972?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522166089959516782391820191%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fblog.%2522%257D&request_id=166089959516782391820191&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~blog~first_rank_ecpm_v1~rank_v31_ecpm-1-125027972-null-null.nonecase&utm_term=Nvm&spm=1018.2226.3001.4450
+
+vue在线编译：https://vue-next-template-explorer.netlify.app/#eyJzcmMiOiI8ZGl2PlxyXG4gICAgPGRpdj4gXHJcbiAgICAgICAgIDxzZWN0aW9uPnRlc3Q8L3NlY3Rpb24+XHJcbiAgICAgIDwvZGl2PiAgXHJcbjwvZGl2PiIsIm9wdGlvbnMiOnt9fQ==
+
+虚拟dom和diff算法：
+https://xiaoman.blog.csdn.net/article/details/122778560
---
+++ src/components/vue_command.vue
@@ -0,0 +1,74 @@
+<template>
+<div>{{ mes.split(',') }}</div>
+<div v-for="(item,index) in arr" :key="index">item:{{item}}</div>
+<div class="on">
+  <!-- 阻止冒泡 -->
+    <div @click="parent">
+    <div @click.stop="child">child</div>
+  </div>
+  <form action="/">
+    <!-- .prevent:阻止表单提交按钮 -->
+    <button type="submit" @click.prevent="submit">submit</button>
+  </form>
+</div>
+<div class="bind1" :style="style">
+ <p>v-bind</p>
+<div :class="flag1">{{flag1}}</div>
+<div :class="[flag ? 'other':'h']">12323</div>
+</div>
+<div class="model">
+  <input v-model="message">
+  {{message}}
+</div>
+</template>
+<script setup lang="ts">
+import {ref} from 'vue'
+// 模板字符串
+const mes:string = "wo,shi,dokidoki"
+// v-for
+const arr:Array<number> = [1.,2,3,54,6]
+// v-on
+const parent = () => {
+  console.log('parent');
+}
+const child = () => {
+  console.log('this child')
+}
+
+
+const submit = () => {
+  console.log('child');
+}
+// v-bind
+type Style ={
+  height:string
+  color:string
+}
+const style:Style ={
+  height:'100px',
+  color:'red'
+}
+
+type Cls = {
+  other:boolean
+  h:boolean
+}
+const flag1:Cls = {
+  other:true,
+  h:false
+}
+const flag:boolean = false
+// v-model
+const message = ref('v-model')
+</script>
+<style scoped>
+.bind1{
+  border: 2px solid cornflowerblue;
+}
+.other{
+  color: seagreen;
+}
+.h{
+  border: 1px solid seagreen;
+}
+</style>
